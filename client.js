ea1f6c67553002fd2e7dad8a374834f4b82d6200
Pascal <pascalvanleeuwen604@gmail.com>
2019-12-04 20:30:05 +0000

    Initial commit with server and client files.

---
+++ client.js
@@ -0,0 +1,8 @@
+const net = require('net');
+
+const conn = net.createConnection({
+  host: 'SERVER IP ADDRESS HERE', // change to IP address
+  port: 3000
+});
+
+conn.setEncoding('utf8'); // interpret data as text
